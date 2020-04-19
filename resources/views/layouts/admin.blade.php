95aa8066590b8e3831f25251193d355c8be6d52e
Ahmad Nazirul <zie_777@live.com>
2020-04-19 11:59:59 +0700

    Clean Up

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -218,7 +218,6 @@
 
 </div>
 
-<!-- Scripts -->
 <script src="{{ asset('js/manifest.js') }}"></script>
 <script src="{{ asset('js/vendor.js') }}"></script>
 <script src="{{ asset('js/admin.js') }}"></script>
