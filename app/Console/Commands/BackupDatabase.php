69c5a3592bb4127bbec84dce21bbfa4de09533e6
Ahmad Nazirul <zie_777@live.com>
2020-08-13 10:37:12 +0700

    temporary disable

--- app/Console/Commands/BackupDatabase.php
+++ app/Console/Commands/BackupDatabase.php
@@ -15,40 +15,40 @@ class BackupDatabase extends Command
     protected $description = 'Backup the database';
 
     protected $process;
-
+    private $backupdate;
+    private $backupname;
+    private $backuppass;
     public function __construct()
     {
         parent::__construct();
-        $backupdate = Carbon::now()->subDays(2)->format('Ymd');
-        $backupname = $backupdate.'-bilcocsv';
-        $backuppass = Str::random(125);
-
-
+        $this->backupdate = Carbon::now()->subDays(2)->format('Ymd');
+        $this->backupname = $this->backupdate.'-bilcocsv';
+        $this->backuppass = Str::random(125);
         $x = sprintf('mysqldump --host="%s" -u"%s" -p"%s" %s --skip-lock-tables --single-transaction --quick | gzip > %s && rar a -hp%s %s.rar %s  && gupload %s.rar --config default=~/.gdriveunli.conf && ls %s/csv/*',
             config('database.connections.mysql.host'),
             config('database.connections.mysql.username'),
             config('database.connections.mysql.password'),
             config('database.connections.mysql.database'),
             storage_path('app/backups/backup-' . Carbon::now()->format('Y-m-d') . '.gz'),
-	        $backuppass,
-            $backupname,
-            storage_path('app/bilcollection/csv/'.$backupdate.'_*'),
-	    $backupname,
+	        $this->backuppass,
+            $this->backupname,
+            storage_path('app/bilcollection/csv/'.$this->backupdate.'_*'),
+	    $this->backupname,
 	    storage_path('app/bilcollection')
         );
         echo $x;
 
         $this->process = Process::fromShellCommandline($x);
-        BackupMan::create(array(
-            'backupName'=> $backupname.'.rar',
-            'backupPassword' => $backuppass
-        ));
     }
 
     public function handle()
     {
         try {
             $this->process->mustRun();
+            BackupMan::create(array(
+                'backupName'=> $this->backupname.'.rar',
+                'backupPassword' => $this->backuppass
+            ));
             $this->info('The backup has been proceed successfully.');
         } catch (ProcessFailedException $exception) {
             echo ($exception);
