4f9590432587c88f1b282efcac8402b2204a6d57
Ahmad Nazirul <zie_777@live.com>
2020-08-13 09:38:53 +0700

    temporary disable

--- app/Console/Commands/BackupDatabase.php
+++ app/Console/Commands/BackupDatabase.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Console\Commands;
+use App\BackupMan;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Symfony\Component\Process\Process;
@@ -17,14 +18,28 @@ class BackupDatabase extends Command
     public function __construct()
     {
         parent::__construct();
-        $x = sprintf('mysqldump --host="%s" -u"%s" -p"%s" %s --skip-lock-tables --single-transaction --quick | gzip > %s && rm %s/csv/*',
+        $backupdate = Carbon::now()->subDays(2)->format('Ymd');
+        $backupname = $backupdate.'-bilcocsv.rar';
+        $backuppass = Str::random(125);
+
+
+        $x = sprintf('mysqldump --host="%s" -u"%s" -p"%s" %s --skip-lock-tables --single-transaction --quick | gzip > %s 
+            && rar a -hp%s %s.rar %s  && gupload log.log --config default=~/.gdriveunli.conf && ls %s/csv/*',
             config('database.connections.mysql.host'),
             config('database.connections.mysql.username'),
             config('database.connections.mysql.password'),
             config('database.connections.mysql.database'),
             storage_path('app/backups/backup-' . Carbon::now()->format('Y-m-d') . '.gz'),
+	        $backuppass,
+            $backupname,
+            storage_path('app/bilcollection/csv/'.$backupdate.'_*'),
+	    $backupname,
 	    storage_path('app/bilcollection')
         );
+        BackupMan::create(array(
+            'backupName'=> $backupname,
+            'backupPassword' => $backuppass
+        ));
         $this->process = Process::fromShellCommandline($x);
     }
 
