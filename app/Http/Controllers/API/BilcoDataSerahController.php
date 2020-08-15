552ee82c9d9c6e2c9e3b0fcc1825b833f2b93b77
Ahmad Nazirul <zie_777@live.com>
2020-08-15 11:07:18 +0700

    update query

--- app/Http/Controllers/API/BilcoDataSerahController.php
+++ app/Http/Controllers/API/BilcoDataSerahController.php
@@ -10,24 +10,36 @@ use App\Http\Controllers\Controller;
 class BilcoDataSerahController extends Controller
 {
     public function fetch(){
-        return DB::table('sabyan_r7s_data.20200802_all as bilco')
-                ->join('olala2.aging_20200803 as aging', function($join)
-                {
-                    //$join->on('aging.msisdn', '=', 'bilco.msisdn');
-                    $join->on('aging.account','=','bilco.account_number');
-                    //$join->on('aging.bill_cycle','=','bilco.bill_cycle');
-                })
-                ->join('olala2.cm_active_unique as cmactive', function($join)
-                {
-                    //$join->on('cmactive.msisdn', '=', 'bilco.msisdn');
-                    $join->on('cmactive.customer_id','=','bilco.account_number');
-                    //$join->on('cmactive.billcycle','=','bilco.bill_cycle');
-                })
-                ->select('aging.account','aging.customer_id','bilco.periode','aging.msisdn','aging.bill_cycle','bilco.regional','bilco.bucket_4','bilco.bucket_3','bilco.bucket_2','bilco.bucket_1',
-                    'aging.grapari','aging.hlr_city','aging.bbs','aging.bbs_name','aging.bbs_company_name','aging.bbs_first_address','aging.bbs_second_address',
-                    'cmactive.customer_address','bbs_city','cmactive.customer_city','aging.bbs_zip_code','aging.aging_cust_subtype','aging.bbs_pay_type',
-                    'aging.bbs_RT','aging.aging_status_subscribe','aging.blocking_status','aging.note','cmactive.customer_phone')
-                ->get();
+        DB::enableQueryLog();
+        echo 'x';
+        $x= DB::table('olala2.cm_active_unique as cmactive')
+            //->table('sabyan_r7s_data.20200802_all as bilco'))
+            ->leftJoin('olala2.aging_20200803 as aging', function($join)
+            {
+                $join->on('cmactive.customer_id','=','aging.customer_id');
+            })
+            ->leftJoin('sabyan_r7s_data.20200802_all as bilco', function($join)
+            {
+                $join->on('aging.account','=','bilco.account_number');
+            })
+            ->select('aging.account','aging.customer_id','bilco.periode','aging.msisdn','aging.bill_cycle','bilco.regional','bilco.bucket_4','bilco.bucket_3','bilco.bucket_2','bilco.bucket_1',
+                'aging.grapari','aging.hlr_city','aging.bbs','aging.bbs_name','aging.bbs_company_name','aging.bbs_first_address','aging.bbs_second_address',
+                'cmactive.customer_address','bbs_city','cmactive.customer_city','aging.bbs_zip_code','aging.aging_cust_subtype','aging.bbs_pay_type',
+                'aging.bbs_RT','aging.aging_status_subscribe','aging.blocking_status','aging.note','cmactive.customer_phone')
+            ->where('aging.bucket_2','>',12500)
+            ->where('aging.bucket_1','>',0)
+            ->where('aging.bucket_3','<=',0)
+            ->where('aging.bucket_4','<=',0)
+            ->where('aging.total_outstanding','>=',50000)
+            ->where('aging.aging_cust_subtype','=','Consumer Reguler')
+            ->where(function($query){
+                $query->orWhere('aging.bill_cycle', '=', 1);
+                $query->orWhere('aging.bill_cycle', '=', 6);
+                $query->orWhere('aging.bill_cycle', '=', 11);
+            })
+            ->get();
+        dd(DB::getQueryLog());
+        return null;
     }
     /**
      * Display a listing of the resource.
