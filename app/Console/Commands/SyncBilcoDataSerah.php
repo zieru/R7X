5d1ea51a117dcd692968c08a0521d957c5dbca5a
Ahmad Nazirul <zie_777@live.com>
2020-08-17 21:40:32 +0700

    latest

--- app/Console/Commands/SyncBilcoDataSerah.php
+++ app/Console/Commands/SyncBilcoDataSerah.php
@@ -2,10 +2,13 @@
 
 namespace App\Console\Commands;
 
+use App\Notifier;
 use App\BilcoDataSerah;
 use App\Http\Controllers\API\BilcoDataSerahController;
 use Illuminate\Console\Command;
 use phpDocumentor\Reflection\DocBlock\Tags\Var_;
+use Carbon\Carbon;
+use App\Importer;
 
 class SyncBilcoDataSerah extends Command
 {
@@ -42,8 +45,24 @@ class SyncBilcoDataSerah extends Command
         $x = array();
         $controller = new BilcoDataSerahController();
         echo 'Query started';
-        $cx= $controller->fetch();
+	$date = Carbon::now()->subDays(2);
+	Notifier::create([
+                    'type' => 'DataSerahImport',
+                    'subject' => 'DataSerah Import file',
+                    'message' => 'Import dataserah started',
+                ]);
+        $cx= $controller->fetch($date);
         $this->info('Query finished');
+Notifier::create([
+                    'type' => 'DataSerahImport',
+                    'subject' => 'DataSerah Import processed',
+                    'message' => 'Import dataserah processed'
+                ]);
+Importer::create(array(
+      'importedRow'=>0,
+      'storedRow'=>0,
+      'status' => 'QUEUE'
+    ));
         foreach($cx as $row){
             $i =  (array) $row;
             $i['cek_cp'] = true;
@@ -80,7 +99,7 @@ class SyncBilcoDataSerah extends Command
             $i['bucket_1']= $row->bucket_1;
 
             */
-            $i['import_batch']= 99;
+            $i['import_batch']= $importer->id;
             $i['total_outstanding'] = $row->bucket_4 + $row->bucket_3 + $row->bucket_2 + $row->bucket_1;
             $x[] = $i;
         }
@@ -91,7 +110,13 @@ class SyncBilcoDataSerah extends Command
         {
             BilcoDataSerah::insert($chunk->toArray());
         }
-
+$importer->status = "Finish";
+    $importer->save();
+Notifier::create([
+                    'type' => 'DataSerahImport',
+                    'subject' => 'DataSerah Import finished',
+                    'message' => 'Import dataserah finished'
+                ]);
         $this->info('Done');
         $this->info($this->argument('periode'));
         return 0;
--- app/Http/Controllers/API/BilcoDataSerahController.php
+++ app/Http/Controllers/API/BilcoDataSerahController.php
@@ -9,29 +9,24 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 class BilcoDataSerahController extends Controller
 {
-    public function fetch(){
+    public function fetch($date){
         DB::enableQueryLog();
-        echo 'x';
+$bilcodate = $date;
+$agingdate = $date->addDays(1);
+
         $x= DB::table('olala2.cm_active_unique as cmactive')
-            //->table('sabyan_r7s_data.20200802_all as bilco'))
-            ->leftJoin('olala2.aging_20200814 as aging', function($join)
+            ->leftJoin('olala2.aging_'.$agingdate->format('Ymd').' as aging', function($join)
             {
                 $join->on('cmactive.customer_id','=','aging.customer_id');
             })
-            ->leftJoin('sabyan_r7s_data.20200813_Sumatra as bilco', function($join)
+            ->leftJoin('sabyan_r7s_data.'.$bilcodate->format('Ymd').'_Sumatra as bilco', function($join)
             {
                 $join->on('aging.account','=','bilco.account_number');
             })
-            ->select('aging.account','aging.customer_id','2020-08-15 AS periode','aging.msisdn','aging.bill_cycle','aging.regional','aging.grapari','bilco.regional AS hlr_region','aging.hlr_city','aging.bbs','aging.bbs_name','aging.bbs_company_name','aging.bbs_first_address','aging.bbs_second_address',
+            ->select('aging.account','aging.customer_id','bilco.periode','aging.msisdn','aging.bill_cycle','aging.regional','aging.grapari','bilco.regional AS hlr_region','aging.hlr_city','aging.bbs','aging.bbs_name','aging.bbs_company_name','aging.bbs_first_address','aging.bbs_second_address',
                 'cmactive.customer_address as cb_address','bbs_city','cmactive.customer_city AS cb_city','aging.bbs_zip_code','aging.aging_cust_subtype','aging.bbs_pay_type',
-                'aging.bbs_RT','bilco.bill_amount_4','bilco.bill_amount_3','bilco.bill_amount_2','bilco.bill_amount_1',
-                'bilco.bucket_4','bilco.bucket_3','bilco.bucket_2','bilco.bucket_1','aging.aging_status_subscribe','aging.blocking_status','aging.note','cmactive.customer_phone')
-            ->where('aging.bucket_2','>',12500)
-            ->where('aging.bucket_1','>',0)
-            ->where('aging.bucket_3','<=',0)
-            ->where('aging.bucket_4','<=',0)
-            ->where('aging.total_outstanding','>=',50000)
-            ->where('aging.aging_cust_subtype','=','Consumer Reguler')
+                'aging.bbs_RT','aging.bill_amount_04','aging.bill_amount_03','aging.bill_amount_02','aging.bill_amount_01',
+                'aging.bucket_4','aging.bucket_3','aging.bucket_2','aging.bucket_1','aging.aging_status_subscribe','aging.blocking_status','aging.note','cmactive.customer_phone')
             ->where(function($query){
                 $query->orWhere('aging.bill_cycle', '=', 1);
                 $query->orWhere('aging.bill_cycle', '=', 6);
