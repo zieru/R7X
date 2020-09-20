4b3ebb62413aa36e27e57d03abd648bec47edba7
Ahmad Nazirul <zie_777@live.com>
2020-09-21 03:20:58 +0700

    fix typos

--- routes/api.php
+++ routes/api.php
@@ -37,7 +37,7 @@ Route::prefix('telegram')->group(function(){
 Route::apiResource('notifier', 'NotifierController');
 Route::prefix('v1')->middleware(['CheckClientCredentials','auth:api'])->group(function() {
     Route::apiResource('bilcodataserah', 'API\BilcoDataSerahController');
-    Route::apiResource('bilcodataserahcekbayar', 'API\BilcoDataSerahCekBayarController');
+    Route::apiResource('bilcodataserahcekbayar', 'API\BilcodataserahCekBayarController');
     Route::get('bilcodataserahkpi', 'API\BilcoDataSerahController@getKpi');
     Route::get('bilcodataserahchart', 'API\BilcoDataSerahController@chart');
     Route::get('bilcodataserahexport', 'API\BilcoDataSerahController@export');
