80e1e66cc14b690a7e18d2e3fa1af306e17abc27
Ahmad Nazirul <zie_777@live.com>
2020-09-28 02:30:31 +0700

    next

--- app/Http/Controllers/API/BilcodataserahCekBayarController.php
+++ app/Http/Controllers/API/BilcodataserahCekBayarController.php
@@ -16,7 +16,7 @@ class BilcodataserahCekBayarController extends Controller
 {
     public function fetch(){
         DB::enableQueryLog();
-        $date = '20200913';
+        $date = '20200813';
         $adate = Carbon::createFromFormat('Ymd', $date);
         $bdate = Carbon::createFromFormat('Ymd', $date)->addDay(1);
         $x= DB::table('sabyan_r7s.bilco_data_serahs AS a')
