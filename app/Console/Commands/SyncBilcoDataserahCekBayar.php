9ced5564456f367380a666e483af6bd4b22a56be
Ahmad Nazirul <zie_777@live.com>
2020-09-30 09:17:18 +0700

    latest

--- app/Console/Commands/SyncBilcoDataserahCekBayar.php
+++ app/Console/Commands/SyncBilcoDataserahCekBayar.php
@@ -4,8 +4,11 @@ namespace App\Console\Commands;
 
 use App\Http\Controllers\API\BilcodataserahCekBayarController;
 use App\Models\BilcodataserahCekBayar;
+use App\Models\SyncBilcoDataserahCekBayarLog;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class SyncBilcoDataserahCekBayar extends Command
 {
@@ -31,6 +34,157 @@ class SyncBilcoDataserahCekBayar extends Command
     public function __construct()
     {
         parent::__construct();
+    }
+
+    public function update($date,$tahap){
+        $basedate = Carbon::createFromFormat('Ymd',$date->format('Ymd'));
+        $x =  $date->format('Ymd');
+        $date = [$date,$date->endOfMonth()];
+        $existtable = [];
+        while($x <= $date[1]->format('Ymd')){
+            $table = sprintf('%s_Sumatra',$x);
+            $exist = Schema::connection('mysql2')->hasTable($table);
+            echo 'cek :'.$table;
+            echo $exist . PHP_EOL;
+            if($exist == 1){
+                $existtable[] = $x;
+            }
+            $x++;
+        }
+
+        foreach ($existtable as $row){
+            //BilcodataserahCekBayar::where('tahap_date',$basedate->format('Ymd'));
+            if($row != $basedate->format('Ymd')){
+                $x= DB::table('sabyan_r7s.bilcodataserah_cek_bayars AS a')
+                    ->select(DB::raw(
+                        'a.tahap_date,
+                        a.tahap_periode,
+                        a.hlr_region,
+                    a.account,
+                    a.msisdn,
+                    a.customer_id,
+                    a.b30,
+                    a.b60,
+                    a.b90,
+                    a.b120,
+                    b.bucket_2 as c30,
+                    b.bucket_3 as c60,
+                    b.bucket_4 as c90,
+                    b.bucket_5 as c120,
+                    a.bill_cycle as bill_cycle,
+                    a.hlr_region as hlr_region')
+                    )
+                    ->Join('sabyan_r7s_data.'.$row.'_Sumatra as b', function($join)
+                    {
+                        $join->on('a.account','=','b.account_number');
+                    })
+                    ->where('a.tahap_date',$basedate->format('Y-m-d'))
+                    ->where('a.tahap_periode',$tahap);
+
+                echo $basedate->format('Y-m-d');
+                foreach ($x->get()->toArray() as $y){
+                    $y = (array) $y;
+                    if($y['c30'] != $y['b30']){
+                        //BilcodataserahCekBayar::
+                        $insert = array(
+                            'periode' => $y['tahap_date'],
+                            'periode_upd' => $row,
+                            'tahap' => $y['tahap_periode'],
+                            'account' => $y['account'],
+                            'customer_id' => $y['customer_id'],
+                            'msisdn' => $y['msisdn'],
+                            'hlr_region' => $y['hlr_region'],
+                            'nominal_bayar' => $y['b30'] - $y['c30'],
+                            'kpi' => 30,
+                            'import_batch' => 0
+                        );
+                        if($y['c30']==0){
+                            $insert['detil_pembayaran'] = sprintf('Dibayar penuh pada %s total tagihan dibayar = %s', $row, $y['b30']+$y['c30']);
+                        }else{
+                            $insert['detil_pembayaran'] = sprintf('Dibayar partial pada %s total tagihan dibayar = %s', $row, $y['b30']+$y['c30']);
+                        }
+                        SyncBilcoDataserahCekBayarLog::insert($insert);
+                        BilcodataserahCekBayar::where('tahap_date',$y['tahap_date'])
+                            ->where('tahap_periode', $tahap)
+                            ->where('account', $y['account'])
+                            ->update(['b30' => $y['c30']]);
+                    }
+                    if($y['c60'] != $y['b60']){
+                        $insert = array(
+                            'periode' => $y['tahap_date'],
+                            'periode_upd' => $row,
+                            'tahap' => $y['tahap_periode'],
+                            'account' => $y['account'],
+                            'customer_id' => $y['customer_id'],
+                            'msisdn' => $y['msisdn'],
+                            'hlr_region' => $y['hlr_region'],
+                            'nominal_bayar' => $y['b60'] - $y['c60'],
+                            'kpi' => 60,
+                            'import_batch' => 0
+                        );
+                        if($y['c60']==0){
+                            $insert['detil_pembayaran'] = sprintf('Dibayar penuh pada %s total tagihan dibayar = %s', $row, $y['b60']+$y['c60']);
+                        }else{
+                            $insert['detil_pembayaran'] = sprintf('Dibayar partial pada %s total tagihan dibayar = %s', $row, $y['b60']+$y['c60']);
+                        }
+                        SyncBilcoDataserahCekBayarLog::insert($insert);
+                        BilcodataserahCekBayar::where('tahap_date',$y['tahap_date'])
+                            ->where('tahap_periode', $tahap)
+                            ->where('account', $y['account'])
+                            ->update(['b60' => $y['c60']]);
+                    }
+                    if($y['c90'] != $y['b90']){
+                        $insert = array(
+                            'periode' => $y['tahap_date'],
+                            'periode_upd' => $row,
+                            'tahap' => $y['tahap_periode'],
+                            'account' => $y['account'],
+                            'customer_id' => $y['customer_id'],
+                            'msisdn' => $y['msisdn'],
+                            'hlr_region' => $y['hlr_region'],
+                            'nominal_bayar' => $y['b90'] - $y['c90'],
+                            'kpi' => 90,
+                            'import_batch' => 0
+                        );
+                        if($y['c90']==0){
+                            $insert['detil_pembayaran'] = sprintf('Dibayar penuh pada %s total tagihan dibayar = %s', $row, $y['b90']+$y['c90']);
+                        }else{
+                            $insert['detil_pembayaran'] = sprintf('Dibayar partial pada %s total tagihan dibayar = %s', $row, $y['b90']+$y['c90']);
+                        }
+                        SyncBilcoDataserahCekBayarLog::insert($insert);
+                        BilcodataserahCekBayar::where('tahap_date',$y['tahap_date'])
+                            ->where('tahap_periode', $tahap)
+                            ->where('account', $y['account'])
+                            ->update(['b90' => $y['c90']]);
+                    }
+                    if($y['c120'] != $y['b120']){
+                        $insert = array(
+                            'periode' => $y['tahap_date'],
+                            'periode_upd' => $row,
+                            'tahap' => $y['tahap_periode'],
+                            'account' => $y['account'],
+                            'customer_id' => $y['customer_id'],
+                            'msisdn' => $y['msisdn'],
+                            'hlr_region' => $y['hlr_region'],
+                            'nominal_bayar' => $y['b120'] - $y['c120'],
+                            'kpi' => 120,
+                            'import_batch' => 0
+                        );
+                        if($y['c120']==0){
+                            $insert['detil_pembayaran'] = sprintf('Dibayar penuh pada %s total tagihan dibayar = %s', $row, $y['b120']+$y['c120']);
+                        }else{
+                            $insert['detil_pembayaran'] = sprintf('Dibayar partial pada %s total tagihan dibayar = %s', $row, $y['b120']+$y['c120']);
+                        }
+                        SyncBilcoDataserahCekBayarLog::insert($insert);
+                        BilcodataserahCekBayar::where('tahap_date',$y['tahap_date'])
+                            ->where('tahap_periode', $tahap)
+                            ->where('account', $y['account'])
+                            ->update(['b120' => $y['c120']]);
+                    }
+                }
+            }
+
+        }
     }
 
     /**
@@ -90,7 +244,7 @@ class SyncBilcoDataserahCekBayar extends Command
                 BilcodataserahCekBayar::insert($row);
             }
         }else{
-
+            $this->update($date,$tahap);
         }
         return 0;
     }
