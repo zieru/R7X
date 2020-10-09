fc27c323d003b7ecc24d47053d7f6706b027d2e6
Ahmad Nazirul <zie_777@live.com>
2020-10-09 11:29:56 +0700

    update

--- app/Http/Controllers/API/BilcoDataSerahController.php
+++ app/Http/Controllers/API/BilcoDataSerahController.php
@@ -103,7 +103,7 @@ class BilcoDataSerahController extends Controller
             ->select('aging.account','aging.customer_id','bilco.periode','aging.msisdn','aging.bill_cycle','aging.regional','aging.grapari','bilco.regional AS hlr_region','aging.hlr_city','aging.bbs','aging.bbs_name','aging.bbs_company_name','aging.bbs_first_address','aging.bbs_second_address',
                 'cmactive.customer_address as cb_address','bbs_city','cmactive.customer_city AS cb_city','aging.bbs_zip_code','aging.bbs_pay_type',
                 'aging.bbs_RT','aging.bill_amount_04','aging.bill_amount_03','aging.bill_amount_02','aging.bill_amount_01',
-                'aging.bucket_4','aging.bucket_3','aging.bucket_2','aging.bucket_1','aging.blocking_status','aging.note','cmactive.customer_phone','bilco.tahap');
+                'aging.bucket_4','aging.bucket_3','aging.bucket_2','aging.bucket_1','aging.blocking_status','aging.note','cmactive.customer_phone');
         if($tahap == 1){
             $x->where(function($query) {
                 $query->where(function ($query)     {
