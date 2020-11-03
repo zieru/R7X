ad8f75102cde5eade61e2b63f8e66e0fd62ec83d
Ahmad Nazirul <zie_777@live.com>
2020-11-03 07:25:09 +0700

    =latest

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
         config(['app.locale' => 'id']);
         //Carbon::setLocale('id');
         Carbon::setLocale('id');
-        date_default_timezone_set('Asia/Jakarta');
+        //date_default_timezone_set('Asia/Jakarta');
         Schema::defaultStringLength(191);
     }
 
