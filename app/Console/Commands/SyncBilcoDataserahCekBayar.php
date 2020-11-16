444e706cbd325d27d2deef9c655ea25557e6e65f
Ahmad Nazirul <zie_777@live.com>
2020-11-16 12:56:22 +0700

    x

--- app/Console/Commands/SyncBilcoDataserahCekBayar.php
+++ app/Console/Commands/SyncBilcoDataserahCekBayar.php
@@ -50,7 +50,7 @@ class SyncBilcoDataserahCekBayar extends Command
         //$basedate = Carbon::createFromFormat('Ymd',$date->format('Ymd'));
         $x =  Carbon::createFromFormat('Ymd',$basedate->format('Ymd'))->format("Ymd");
         $x_endofmonth = Carbon::createFromFormat('Ymd',$date->format('Ymd'))->endOfMonth();
-        $x_endofmonth = Carbon::createFromFormat('Ymd','20201109')->endOfMonth();
+        //$x_endofmonth = Carbon::createFromFormat('Ymd','20201109')->endOfMonth();
         $this->info(sprintf('Job #%d update cekbayar from %s until %s',$importer->id,$x,$x_endofmonth->format('Ymd')));
         $date = [$date,$x_endofmonth];
         $existtable = [];
