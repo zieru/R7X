acd036f30a81f9b43739527529fb7c066df1b6ed
Ahmad Nazirul <zie_777@live.com>
2020-11-16 13:17:04 +0700

    fix UsersController duplicate file

--- app/SyncBilcoDataserahCekBayarLog.php
+++ app/SyncBilcoDataserahCekBayarLog.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class SyncBilcoDataserahCekBayarLog extends Model
 {
+    protected $primaryKey = null;
     public $incrementing = false;
     protected $table = 'sync_bilco_dataserah_cek_bayar_logs';
     protected $guarded = [];
